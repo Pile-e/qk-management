6de6977ecafd36d47664f9568d2fb7a6a6ddf5aa
圆周率 <308749894@qq.com>
2026-04-19 16:36:06 +0800

    新增线索

--- qk-management/src/main/java/com/qk/controller/ClueController.java
+++ qk-management/src/main/java/com/qk/controller/ClueController.java
@@ -7,9 +7,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @Slf4j
-@RestController("/clues")
+@RestController
+@RequestMapping("/clues")
 public class ClueController {
+    @Autowired
+    private ClueService clueService;
+
+    /**
+     * 新增线索
+     *
+     * @param clue
+     * @return
+     */
+    @PostMapping
+    private Result addClue(@RequestBody Clue clue) {
+        clue.setCreateTime(LocalDateTime.now());
+        clue.setUpdateTime(LocalDateTime.now());
+        clue.setStatus(1);
+        clueService.save(clue);
+        return Result.success();
+    }
 }
